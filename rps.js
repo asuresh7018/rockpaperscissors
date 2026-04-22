c97884dd3fdae9285fbe95c914d1e5c20682eafc
Arjun Suresh <asuresh7018@gmail.com>
2026-04-22 08:15:09 +0000

    Functioning game with GUI

--- rps.js
+++ rps.js
@@ -21,51 +21,52 @@ let computerScore = 0;
 // Play one round of rock, paper, scissors
 function playRound(humanChoice, computerChoice) {
     humanChoice = String(humanChoice).toLowerCase();
+    const resultElem = document.querySelector("#results");
     switch(humanChoice) {
         case "rock": {
             if (computerChoice === "rock") {
-                console.log("Draw!");
+                resultElem.innerText = "Draw!";
                 break;
             }
             else if (computerChoice === "scissors") {
-                console.log("You win! Rock beats Scissors.");
+                resultElem.innerText = "You win! Rock beats Scissors.";
                 humanScore++;
                 break;
             }
             else {
-                console.log("You lose! Paper beats rock.");
+                resultElem.innerText = "You lose! Paper beats rock.";
                 computerScore++;
                 break;
             }
         }
         case "paper": {
             if (computerChoice === "paper") {
-                console.log("Draw!");
+                resultElem.innerText = "Draw!";
                 break;
             }
             else if (computerChoice === "rock") {
-                console.log("You win! Paper beats Rock.");
+                resultElem.innerText = "You win! Paper beats Rock.";
                 humanScore++;
                 break;
             }
             else {
-                console.log("You lose! Scissors beats Paper.");
+                resultElem.innerText = "You lose! Scissors beats Paper.";
                 computerScore++;
                 break;
             }
         }
         case "scissors": {
             if (computerChoice === "scissors") {
-                console.log("Draw!");
+                resultElem.innerText = "Draw!";
                 break;
             }
             else if (computerChoice === "paper") {
-                console.log("You win! Scissors beats Paper.");
+                resultElem.innerText = "You win! Scissors beats Paper.";
                 humanScore++;
                 break;
             }
             else {
-                console.log("You lose! Rock beats Scissors.");
+                resultElem.innerText = "You lose! Rock beats Scissors.";
                 computerScore++;
                 break;
             }
@@ -73,24 +74,28 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-const rounds = [1,2,3,4,5];
-
-for (const round of rounds) {
-    console.log("Round " + round);
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
+function doRound(humanChoice) {
+    if (!(humanScore === 5 || computerScore === 5)) {
+        const cpuChoice = getComputerChoice();
+        playRound(humanChoice, cpuChoice);
+        const scoreElem = document.querySelector("#score");
+        scoreElem.innerText =  `${humanScore}-${computerScore}`;
+    }  
+    if (humanScore === 5 || computerScore === 5) {
+        const resultElem = document.querySelector("#results");
+        if (humanScore > computerScore) {
+            resultElem.innerText = "Game Over! You win!";
+        }
+        else {
+            resultElem.innerText = "Game Over! You lose!";
+        }
+    }
 }
 
-if (humanScore > computerScore) {
-    console.log("Final result: You win!");
-}
-else if (computerScore > humanScore) {
-    console.log("Final result: You lose!");
-}
-else {
-    console.log("Final result: Draw!");
-}
-console.log(`Final score was ${humanScore}-${computerScore}`);
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        doRound(button.id);
+    });
+    }
+)
